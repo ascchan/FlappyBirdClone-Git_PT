357bf8874b2a7f4025e3a08d937312d9d3599211
Anthony Chan <achan@acm.org>
2026-07-17 23:08:04 -0400

    Completed Lab6 for input systems

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdController : MonoBehaviour
 {
@@ -7,26 +8,73 @@ public class BirdController : MonoBehaviour
 
     void Update()
     {
-        if(Input.touchCount > 0)
-        {
-            Touch touchInfo = Input.GetTouch(0);
+//        if(Input.touchCount > 0)
+//        {
+//            Touch touchInfo = Input.GetTouch(0);
 
-            if(touchInfo.phase == TouchPhase.Began)
-            {
-                birdRigidbody.linearVelocityY = jumpForce;
+//            if(touchInfo.phase == TouchPhase.Began)
+//            {
+//                birdRigidbody.linearVelocityY = jumpForce;
 //                Debug.Log("Figner started touching");
-            }
+//            }
 
-            if(touchInfo.phase == TouchPhase.Ended)
-            {
-                Debug.Log("Figner was released");
-            }
+//            if(touchInfo.phase == TouchPhase.Ended)
+//            {
+//                Debug.Log("Figner was released");
+//            }
 
-            if (touchInfo.phase == TouchPhase.Moved)
-            {
-                Debug.Log("Finger is moving");
-            }
+//            if (touchInfo.phase == TouchPhase.Moved)
+//            {
+//                Debug.Log("Finger is moving");
+//            }
 
+//        }
+    }
+
+    public void JumpBird(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            Debug.Log("Jumping the bird");
+            birdRigidbody.linearVelocityY = jumpForce;
+        }
+        if (ctx.canceled)
+        {
+            Debug.Log("Button was released");
         }
     }
+
+    public void MoveBird(InputAction.CallbackContext ctx)
+    {
+        Vector2 direction = ctx.ReadValue<Vector2>();
+        Debug.Log(direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //disable input
+        //play sound
+        //show game over screen
+        //register score
+
+        /*
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //add score points
+        //play sound
+
+        /*
+        if (collision.gameObject.CompareTag("ScoreZone"))
+        {
+            Debug.Log("Score!");
+        }*/
+
+    }
+
 }
